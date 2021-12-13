cc40b2e4c38fe3ca4e94aea06b9c4f30eabcfd4a
Andres-XL <amovilla1103@gmail.com>
2021-12-13 10:53:36 -0500

    portfolio-rainbow

---
+++ precache-manifest.3cef0555036ea7832d03272fdc5839a3.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "363edc03140c763ee44a64b31525c396",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c71d564ad3c37afe2fd3",
+    "url": "/static/css/main.a86b845b.chunk.css"
+  },
+  {
+    "revision": "1f6dd0db4e839a8ccb7b",
+    "url": "/static/js/2.d4809c1f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d4809c1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c71d564ad3c37afe2fd3",
+    "url": "/static/js/main.536cc860.chunk.js"
+  },
+  {
+    "revision": "420d11f43645d04fc865",
+    "url": "/static/js/runtime-main.9b126251.js"
+  },
+  {
+    "revision": "711f87617c3db1cd1843e727a9837eab",
+    "url": "/static/media/arrow-down.711f8761.svg"
+  },
+  {
+    "revision": "2e0fd72e1e28c1bedc7e25c581fd6e1e",
+    "url": "/static/media/arrow-right.2e0fd72e.svg"
+  },
+  {
+    "revision": "2dd064ae777c2df806652c97ae05fd59",
+    "url": "/static/media/award.2dd064ae.svg"
+  },
+  {
+    "revision": "282ee78f51442ece4f532835713b4281",
+    "url": "/static/media/blob1.282ee78f.svg"
+  },
+  {
+    "revision": "43f684ae04fe1597a7e681095b385c8b",
+    "url": "/static/media/blob2.43f684ae.svg"
+  },
+  {
+    "revision": "09ff278bb410a9fd93dd92ef1f8cb955",
+    "url": "/static/media/blob3.09ff278b.svg"
+  },
+  {
+    "revision": "e8e8f759ef0ad9a3fd65a2a7cb460431",
+    "url": "/static/media/code.e8e8f759.svg"
+  },
+  {
+    "revision": "54b8b93ddf90199a36a52967f690a36c",
+    "url": "/static/media/download.54b8b93d.svg"
+  },
+  {
+    "revision": "5717a6b3b04a551a08bc4e9c69d0cadc",
+    "url": "/static/media/envelope.5717a6b3.svg"
+  },
+  {
+    "revision": "df5f8d691b6283bb694a75ac67152ccc",
+    "url": "/static/media/game1_title.df5f8d69.jpg"
+  },
+  {
+    "revision": "a93fa7de0470a479d53631f01b133702",
+    "url": "/static/media/game2_title.a93fa7de.jpg"
+  },
+  {
+    "revision": "eca39b49f6ac71c9e431f69b4f0fbd98",
+    "url": "/static/media/game3_title.eca39b49.jpg"
+  },
+  {
+    "revision": "a157e901f0c6e6ff5014781233a4e537",
+    "url": "/static/media/github.a157e901.svg"
+  },
+  {
+    "revision": "4d0c91a25775e59aab5d801c57d0f879",
+    "url": "/static/media/grad.4d0c91a2.svg"
+  },
+  {
+    "revision": "99c7d67634e458273abe1479e17f6e86",
+    "url": "/static/media/hamburger-01.99c7d676.svg"
+  },
+  {
+    "revision": "8464d631d2d84a266b7eea944ae372c0",
+    "url": "/static/media/hamburger-02.8464d631.svg"
+  },
+  {
+    "revision": "4a8c97039c92c821bfeae79a2d417d94",
+    "url": "/static/media/hamburger-03.4a8c9703.svg"
+  },
+  {
+    "revision": "5e5a2413318c9941eafdd669ea7cbf07",
+    "url": "/static/media/linkedin.5e5a2413.svg"
+  },
+  {
+    "revision": "5b1341fc390aaac0a4ef370efd851411",
+    "url": "/static/media/pic2.5b1341fc.png"
+  },
+  {
+    "revision": "3c0afe159a63393de30d335f36054fe2",
+    "url": "/static/media/pic4.3c0afe15.png"
+  },
+  {
+    "revision": "da4101a3cd0e0f5320c5149feb9562e9",
+    "url": "/static/media/pic5.da4101a3.png"
+  },
+  {
+    "revision": "d4d4c49b9ccc1292a54fe49d12717834",
+    "url": "/static/media/project1.d4d4c49b.png"
+  },
+  {
+    "revision": "6c782ed19128d7e95f5f604d392254d3",
+    "url": "/static/media/project2.6c782ed1.png"
+  },
+  {
+    "revision": "2048ed845f92f19730589e2bd595c12d",
+    "url": "/static/media/project3.2048ed84.png"
+  },
+  {
+    "revision": "724df17c61de3f5e85f006a262220318",
+    "url": "/static/media/project4.724df17c.png"
+  },
+  {
+    "revision": "12fcee410e53e918d8d56282383af5d4",
+    "url": "/static/media/project5.12fcee41.png"
+  },
+  {
+    "revision": "1df86bff60eadf99cd8091d0264e1fd1",
+    "url": "/static/media/school.1df86bff.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c5954e8bfaf705774e23d30ee6226779.js"
+  "/precache-manifest.3cef0555036ea7832d03272fdc5839a3.js"
 );
 
 self.addEventListener('message', (event) => {
